29d840969fa7cebe7feb0319265689d92dc97c8b
Henrick Adrian <81784316+henrickk@users.noreply.github.com>
2022-11-16 22:13:05 -0300

    update epictask

---
+++ EPICTASK/assets/js/card.js
@@ -0,0 +1,49 @@
+const cores = {
+    analise: "#F06",
+    teste: "#0F9",
+    bd: "#F90",
+    ux: "#FF00E6"
+}
+
+function Card(tarefa) {
+    let card = document.createElement("div")
+    card.classList.add("card")
+
+    let h2 = document.createElement("h2")
+    h2.textContent = tarefa.titulo
+
+    let span = document.createElement("span")
+    span.textContent = tarefa.pontos + " pontos"
+
+    let meter = document.createElement("meter")
+    meter.value = tarefa.pontos
+    meter.min = 0
+    meter.max = 100
+
+    let botaoFinalizar = document.createElement("button")
+    botaoFinalizar.classList.add("botao")
+    botaoFinalizar.textContent = "finalizar"
+    botaoFinalizar.addEventListener('click', () => {
+        botaoFinalizar.disabled = true
+        pontos = pontos + Number(tarefa.pontos)
+        document.querySelector(".pontos").innerHTML = pontos
+    })
+
+    let botaoApagar = document.createElement("button")
+    botaoApagar.classList.add("botao")
+    botaoApagar.textContent = "apagar"
+    botaoApagar.addEventListener('click', () => {
+        card.style.animation = "remove 1s";
+        setTimeout(() => card.remove(), 1000)
+    })//botão para apagar (com animação)
+
+    card.style.borderTop = "1px solid " + cores[tarefa.categoria]
+
+    card.appendChild(h2)
+    card.appendChild(span)
+    card.appendChild(meter)
+    card.appendChild(botaoFinalizar)
+    card.appendChild(botaoApagar)
+
+    return card
+}
---
+++ EPICTASK/assets/js/script.js
@@ -0,0 +1,22 @@
+let pontos = 0
+
+const botao = document.querySelector("#botaoAdicionar")
+
+botao.addEventListener("click", (e) => {
+    e.preventDefault()
+    let titulo = document.querySelector("#titulo").value
+    let pontos = document.querySelector("#pontos").value
+    let categoria = document.querySelector("#categoria").value
+
+    const tarefa = {
+        titulo,
+        pontos,
+        categoria
+    }
+
+    const card = Card(tarefa)
+
+    document.querySelector("#tarefas").appendChild(card)
+
+
+})
